45c5c0130e81f6ba9c99e8b75256f375e441605f
Elena <lena.ksenofontova@mail.ru>
2023-05-25 15:31:40 +0300

    compliting second homework

--- DigitalDesign/Program.cs
+++ DigitalDesign/Program.cs
@@ -1,20 +1,27 @@
-﻿using System.Reflection;
+﻿using System.Diagnostics;
+using System.Reflection;
+using System.Reflection.Metadata;
 using System.Text;
 using TextAnalysisLibrary;
 
 
 class DigitalDesign
 {
-    static void Main()
+
+    public static void Main()
     {
+        Stopwatch stopwatch = new();
+        List<long> timeTests = new(); //Список для измерения времени работы метода
+        int tests = 20; //Количество тестов для измерения времени
+
         // Получение метода через рефлексию
         Type type = typeof(TextAnalysisLibrary.TextAnalysis);
         var methodInfo = type.GetMethod("GetTextAnalysis", BindingFlags.NonPublic | BindingFlags.Instance);
 
-
         // Чтение текстового файла
         Console.Write("Input file path: ");
         string? inputFilename = Console.ReadLine();
+       
         if (!File.Exists(inputFilename))
         {
             Console.WriteLine("File doesn't exist");
@@ -22,28 +29,69 @@ class DigitalDesign
         }
         string text = File.ReadAllText(inputFilename, Encoding.UTF8);
 
+
         if (!String.IsNullOrEmpty(text))
         {
-            // Вызов приватного метода из dll, получение результата и сортировка полученного результата
-            var wordCounts = methodInfo!.Invoke(new TextAnalysis(), new object[] { text }) as Dictionary<string, int>;
-            var sortedDict = wordCounts!.OrderBy(x => -x.Value).ToDictionary(x => x.Key, x => x.Value);
+            Dictionary<string, int>? wordCounts = new();
 
-
-            // Запись результата в файл
-            string outputFilePath = Path.GetFileNameWithoutExtension(inputFilename) + "_wordcount.txt";// Путь к выходному файлу
-           
-            using StreamWriter writer = new(outputFilePath);
-            foreach (var entry in sortedDict)
+            // Вызов приватного однопоточного метода из dll, получение результата и сортировка полученного результата
+            string outputFilePath = Path.GetFileNameWithoutExtension(inputFilename) + "_wordcount.txt";// Путь к выходному файлу однопоточного
+            for (int i = 0; i < tests; i++)
             {
-                writer.WriteLine($"{entry.Key}: {entry.Value}");
+                stopwatch.Restart();
+                wordCounts = methodInfo!.Invoke(new TextAnalysis(), new object[] { text }) as Dictionary<string, int>;
+                stopwatch.Stop();
+                timeTests.Add(stopwatch.ElapsedMilliseconds);
             }
-            Console.WriteLine("Text processing completed. The result is written");
+            Console.WriteLine($"Время работы однопоточного алгоритма: {timeTests.Average()}");
+            var sortedDict = wordCounts!.OrderBy(x => -x.Value).ToDictionary(x => x.Key, x => x.Value);
+            WritingToFile(sortedDict, outputFilePath);
+            timeTests.Clear();
+
+          
+            // Вызов параллельного метода с использованием CurrentDict
+            string outputFilePathParallel3 = Path.GetFileNameWithoutExtension(inputFilename) + "_wordcountParallelCD.txt";// Путь к выходному файлу для параллельной обработки
+
+            for (int i = 0; i < tests; i++)
+            {
+                stopwatch.Restart();
+                wordCounts = TextAnalysis.GetTextAnalysisParallelCD(text).ToDictionary(x => x.Key, x => x.Value);
+                stopwatch.Stop();
+                timeTests.Add(stopwatch.ElapsedMilliseconds);
+            }
+            Console.WriteLine($"Время работы многопоточного алгоритма с использованием CurrentDictionary: {timeTests.Average()}");
+            var sortedDictParallel3 = wordCounts!.OrderBy(x => -x.Value).ToDictionary(x => x.Key, x => x.Value);
+            WritingToFile(sortedDictParallel3, outputFilePathParallel3);
+            timeTests.Clear();
+
+            // Вызов параллельного метода с разделением на части
+            string outputFilePathParallel = Path.GetFileNameWithoutExtension(inputFilename) + "_wordcountParallel.txt";// Путь к выходному файлу для параллельной обработки
+            for (int i = 0; i < tests; i++)
+            {
+                stopwatch.Restart();
+                wordCounts = TextAnalysis.GetTextAnalysisParallel(text);
+                stopwatch.Stop();
+                timeTests.Add(stopwatch.ElapsedMilliseconds);
+            }
+            Console.WriteLine($"Время работы многопоточного алгоритма с разделением на части: {timeTests.Average()}");
+            var sortedDictParallel = wordCounts!.OrderBy(x => -x.Value).ToDictionary(x => x.Key, x => x.Value);
+            WritingToFile(sortedDictParallel, outputFilePathParallel);
+
         }
         else
-        {
             Console.WriteLine("File is empty");
-        }
-        
+
         Console.ReadLine();
+    }
+
+    //Запись результата в файл
+    public static void WritingToFile(Dictionary<string, int> sortedDict, string outputFilePath) 
+    {
+        using StreamWriter writer = new(outputFilePath);
+        foreach (var entry in sortedDict)
+        {
+            writer.WriteLine($"{entry.Key}: {entry.Value}");
+        }
+        Console.WriteLine("Text processing completed. The result is written");
     }
 }
--- TextAnalysisLibrary/TextAnalysis.cs
+++ TextAnalysisLibrary/TextAnalysis.cs
@@ -1,13 +1,15 @@
-﻿namespace TextAnalysisLibrary
+﻿using System.Collections.Concurrent;
+
+namespace TextAnalysisLibrary
 {
     public class TextAnalysis
     {
-        private  Dictionary<string, int> GetTextAnalysis(string text)
+        static char[] delimeters = Enumerable.Range(0, 256).Select(i => (char)i).Where(c => !char.IsLetter(c)).ToArray();
+      
+        //Однопоточный приватный метод
+        private Dictionary<string, int> GetTextAnalysis(string text)
         {
-            Dictionary<string, int> wordCount = new ();
-            var delimeters = Enumerable.Range(0, 256).Select(i => (char)i).Where(c => !char.IsLetter(c)).ToArray();
-
-            //string[] words = text.Split(' ');
+            Dictionary<string, int> wordCount = new();
             string[] words = text.Split(delimeters, StringSplitOptions.RemoveEmptyEntries);
             foreach (string word in words)
             {
@@ -16,6 +18,58 @@
                 else
                     wordCount[word] = 1;
             }
+            return wordCount;
+        }
+
+        //Многопоточные методы  
+          
+        public static ConcurrentDictionary<string, int> GetTextAnalysisParallelCD(string text)
+        {
+            ConcurrentDictionary<string, int> wordCount = new();
+            string[] words = text.Split(delimeters, StringSplitOptions.RemoveEmptyEntries);
+            Parallel.ForEach(words, word =>
+            {
+                wordCount.AddOrUpdate(word, 1, (_, count) => count + 1);
+            });
+            return wordCount;
+        }
+
+
+        public static Dictionary<string, int> GetTextAnalysisParallel(string text)
+        {
+            string[] words = text.Split(delimeters, StringSplitOptions.RemoveEmptyEntries);
+            int processorCount = Environment.ProcessorCount;
+            Dictionary<string, int>[] partialWordCounts = new Dictionary<string, int>[processorCount];
+
+            
+            Parallel.For(0, processorCount, i =>
+            {
+                partialWordCounts[i] = new Dictionary<string, int>();
+                int startIndex = (words.Length / processorCount) * i;
+                int endIndex = (i == processorCount - 1) ? words.Length : (words.Length / processorCount) * (i + 1);
+
+                for (int j = startIndex; j < endIndex; j++)
+                {
+                    string word = words[j];
+                    if (partialWordCounts[i].ContainsKey(word))
+                        partialWordCounts[i][word]++;
+                    else
+                        partialWordCounts[i][word] = 1;
+                }
+            });
+
+            Dictionary<string, int> wordCount = new ();
+            foreach (var partialCount in partialWordCounts)
+            {
+                foreach (var kvp in partialCount)
+                {
+                    if (wordCount.ContainsKey(kvp.Key))
+                        wordCount[kvp.Key] += kvp.Value;
+                    else
+                        wordCount[kvp.Key] = kvp.Value;
+                }
+            }
+
             return wordCount;
         }
     }
